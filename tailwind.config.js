6e668b536e1b0a20117eb1af00b16a070cd566d2
gedeamerta <amerta213bali@gmail.com>
2022-05-17 22:38:02 +0800

    Update Content and CSS; Note: Fix the CSS please!!!

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,12 @@ module.exports = {
     maxWidth: {
       'sm': '400px'
     },
+    colors: {
+      'primary': '#333333',
+      'secondary': '#F4F4F4',
+      'third-color': '#BDC3D6',
+      'gradient': 'rgba(0, 0, 0, 1)'
+    },
     extend: {},
     fontFamily: {
       inter: ["Inter", "sans-serif"],
